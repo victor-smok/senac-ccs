1f1e18464043c73b6e27f764d4537b756cf96336
Anderson Vaz <dev@andersonvaz.com>
2012-08-30 16:15:43 -0300

    primeira abordagem do problema.

--- amigo-secreto/src/main/java/br/com/senac/amigosecreto/JogoAmigoSecreto.java
+++ amigo-secreto/src/main/java/br/com/senac/amigosecreto/JogoAmigoSecreto.java
@@ -2,26 +2,27 @@ package br.com.senac.amigosecreto;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class JogoAmigoSecreto {
 
-    public List<Participante> sortear( String[] nomes ) {
-        List<Participante> participantes = new ArrayList<Participante>();
-        for ( int i = 0; i < nomes.length; i++ ) {
-            Participante participante = new Participante( nomes[i] );
-            participantes.add( participante );
+    public List<Participante> sortear( String[] participantes ) {
+        List<Participante> sorteio = new ArrayList<Participante>();
+        List<Participante> temp = new ArrayList<Participante>();
+        List<String> nomes = new ArrayList<String>( Arrays.asList( participantes ) );
+        for ( String nome : nomes ) {
+            sorteio.add( new Participante( nome ) );
+            temp.add( new Participante( nome ) );
         }
-        Collections.shuffle( participantes );
-        List<Participante> caixa = new ArrayList<Participante>( participantes );
-        for ( Participante participante : participantes ) {
-            caixa.remove( participante ); // para garantir que o participante nao sorteie ele mesmo
-            Collections.shuffle( caixa );
-            Participante amigoSecreto = caixa.get( 0 );
-            caixa.remove( amigoSecreto );
-            caixa.add( participante );
-            participante.setAmigoSecreto( amigoSecreto );
+        while ( !nomes.isEmpty() ) {
+            Participante participante = temp.get( ( (int) ( Math.random() * temp.size() ) ) );
+            String nome = nomes.get( ( (int) ( Math.random() * nomes.size() ) ) );
+            if ( !participante.getNome().equals( nome ) ) {
+                participante.setAmigoSecreto( new Participante( nome ) );
+                nomes.remove( nome );
+                temp.remove( participante );
+            }
         }
-        return participantes;
+        return sorteio;
     }
 }
